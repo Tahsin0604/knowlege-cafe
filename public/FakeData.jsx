6c7b0816cd6cec3421e969f7ff86140688d815ad
Tahsin0604 <natikalamtahsin@gmail.com>
2023-03-31 00:38:41 +0600

    fake data and image

---
+++ public/FakeData.jsx
@@ -0,0 +1,56 @@
+[
+  {
+    "id":1,
+    "authorName":"Kelsey Piper",
+    "blogTitle":"Think AI was impressive last year? Wait until you see what’s coming.",
+    "blogCoverImage":"../src/assets/image/cover-1.jpg",
+    "authorImage":"../src/assets/image/user-1.jpg",
+    "readTime":10,
+    "publishDate":"2023-03-31"
+  },
+  {
+    "id":2,
+    "authorName":"Kelsey Piper",
+    "blogTitle":"Google’s new AI chatbot seems boring. Maybe that’s the point.",
+    "blogCoverImage":"../src/assets/image/cover-2.jpg",
+    "authorImage":"../src/assets/image/user-2.jpg",
+    "readTime":15,
+    "publishDate":"2023-03-28"
+  },
+  {
+    "id":3,
+    "authorName":"Samantha Oltman ",
+    "blogTitle":"Your brain may not be private much longer",
+    "blogCoverImage":"../src/assets/image/cover-3.jpg",
+    "authorImage":"../src/assets/image/user-3.jpg",
+    "readTime":13,
+    "publishDate":"2023-03-29"
+  },
+  {
+    "id":4,
+    "authorName":"Sean Illing",
+    "blogTitle":"Are Elon’s Tweets turning car buyers against Tesla?",
+    "blogCoverImage":"../src/assets/image/cover-4.jpg",
+    "authorImage":"../src/assets/image/user-4.jpg",
+    "readTime":7,
+    "publishDate":"2023-03-25"
+  },
+  {
+    "id":5,
+    "authorName":"Kim Kelly",
+    "blogTitle":"Lots of bad science still gets published. Here’s how we can change that.",
+    "blogCoverImage":"../src/assets/image/cover-5.jpg",
+    "authorImage":"../src/assets/image/user-5.jpg",
+    "readTime":6,
+    "publishDate":"2023-03-28"
+  },
+  {
+    "id":6,
+    "authorName":"Ezra Klein",
+    "blogTitle":"How technology literally changes our brains",
+    "blogCoverImage":"../src/assets/image/cover-6.jpg",
+    "authorImage":"../src/assets/image/user-6.jpg",
+    "readTime":16,
+    "publishDate":"2023-03-25"
+  }
+]

b6f8ad4fa0157f5b59651d4022f3c9bb63715bc8
Tahsin0604 <natikalamtahsin@gmail.com>
2023-03-31 22:21:03 +0600

    final fixed

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -5,7 +5,7 @@ const Blog = () => {
     <div className='px-2 lg:px-28 py-8'>
       <h1 className='mb-8 text-center text-4xl text-slate-700  font-semibold underline decoration-1 decoration-slate-200 underline-offset-8'>Questions Section</h1>
       <h1 className='mb-3 text-2xl text-slate-700  font-semibold'>1. Props vs state</h1>
-      <p className='mb-4 text-lg text-slate-600  font-semibold'>Props is not property of a component. It just used to passed from parent component to child component. <br />
+      <p className='mb-4 text-lg text-slate-600  font-semibold'>Props is not property of a component. It is just used to passed from parent component to child component. Components can not change props.<br />
       state is the property of the component. Component can update state.</p>
       <h1 className='mb-3 text-2xl text-slate-700  font-semibold'>2. How does useState work?</h1>
       <p className='mb-4 text-lg text-slate-600  font-semibold'>useState hook returns an array of two variables: a current state and function to change the state. At declaration we have to give an initial value to current state. when we have to change the state we have to use the function.
